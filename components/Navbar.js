62ff944bd3cf4a04f6aed2f1f3f0f8e54f95c086
Amit Paswan <ak9425391@gmail.com>
2023-10-27 04:10:17 +0530

    bug fixed

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,103 +8,107 @@ import Image from "next/image";
 
 const Navbar = () => {
   const [nav, setnav] = useState(false);
+
   function navhandler() {
     setnav(!nav);
   }
+
   function logohandler() {
     setnav(false);
   }
   return (
     <>
       <div className="flex shadow-cyan-900 bg-black w-full fixed top-[0] z-10  h:[50px] sm:h-[80px] justify-end text-[#00D8FF] items-center flex-row  shadow-sm p-3 md:p-5">
+        {" "}
         {/* <Link href="/" onClick={logohandler}>
-          {" "}
-          <Image
-            className="md:w-32 w-28 sm:w-48 h-24 md:h-fit py-2"
-            width={150}
-            height={100}
-            // src={Logo}
-            alt="logo"
-          />
-        </Link> */}
+                                                                                                  {" "}
+                                                                                                  <Image
+                                                                                                    className="md:w-32 w-28 sm:w-48 h-24 md:h-fit py-2"
+                                                                                                    width={150}
+                                                                                                    height={100}
+                                                                                                    // src={Logo}
+                                                                                                    alt="logo"
+                                                                                                  />
+                                                                                                </Link> */}{" "}
         <div className="lg:flex cursor-poinvscode-file://vscode-app/c:/Users/DELL/AppData/Local/Programs/Microsoft%20VS%20Code/resources/app/out/vs/code/electron-sandbox/workbench/workbench.htmlter  flex-row space-x-12 mx-7 hidden">
           <Link
             href="/Aboutus"
-            className="hover:scale-125 hover:text-cyan-600  ease-in duration-200 text-xl font-semibold"
-          >
-            About
-          </Link>
+            className="hover:scale-125 hover:text-cyan-600  ease-in duration-200 text-xl font-semibold">
+            About{" "}
+          </Link>{" "}
           <Link
             href="/Projects"
-            className="hover:scale-125 hover:text-cyan-600  ease-in duration-200  text-xl font-semibold"
-          >
-            Projects
-          </Link>
+            className="hover:scale-125 hover:text-cyan-600  ease-in duration-200  text-xl font-semibold">
+            Projects{" "}
+          </Link>{" "}
           {/* <Link href="/Certificates" className="hover:scale-125 hover:text-orange-400  ease-in duration-200  text-xl font-semibold">
-          Certificates
-        </Link> */}
+                                                                                          Certificates
+                                                                                        </Link> */}{" "}
           <Link
             href="/Contactus"
-            className="hover:scale-125 hover:text-cyan-600  ease-in duration-200  text-xl font-semibold"
-          >
-            Contact
-          </Link>
-        </div>
+            className="hover:scale-125 hover:text-cyan-600  ease-in duration-200  text-xl font-semibold">
+            Contact{" "}
+          </Link>{" "}
+        </div>{" "}
         <div className="lg:hidden">
+          {" "}
           {nav === false ? (
             <FaChevronDown size={25} onClick={navhandler} />
           ) : (
             <FaChevronUp size={25} onClick={navhandler} />
-          )}
-        </div>
-      </div>
-
+          )}{" "}
+        </div>{" "}
+      </div>{" "}
       <div
         className={
           nav === true
             ? "z-10 flex lg:hidden pt-10 flex-col px-8   space-y-10  items-center h-full bg-black fixed text-[#00D8FF] top-[49.5px] sm:top-[81px]  w-full text-center"
             : "hidden"
-        }
-      >
+        }>
         <Link
           onClick={navhandler}
           href="/Aboutus"
-          className="text-4xl border-b-2 w-full pb-2 font-semibold"
-        >
-          About
-        </Link>
+          className="text-4xl border-b-2 w-full pb-2 font-semibold">
+          About{" "}
+        </Link>{" "}
         <Link
           onClick={navhandler}
           href="/Projects"
-          className="text-4xl border-b-2 w-full pb-2 font-semibold"
-        >
-          Projects
-        </Link>
+          className="text-4xl border-b-2 w-full pb-2 font-semibold">
+          Projects{" "}
+        </Link>{" "}
         {/* <button className="bg-yellow-400 p-4 w-[90%] text-black rounded-[20px]">    <Link onClick={navhandler} href="/Certificates" className="text-4xl font-semibold">
-          Certificates
-        </Link></button> */}
+                                                                                  Certificates
+                                                                                </Link></button> */}{" "}
         <Link
           onClick={navhandler}
           href="/Contactus"
-          className="border-b-2 w-full pb-2 text-4xl font-semibold"
-        >
-          Contact
-        </Link>
+          className="border-b-2 w-full pb-2 text-4xl font-semibold">
+          Contact{" "}
+        </Link>{" "}
         <div className="flex w-full items-center justify-around flex-row">
-        <Buttonrounded link={'https://www.linkedin.com/in/aditya-kaushik-9a836a186/'} title={'Linkedin'}>
-              <FaLinkedinIn color="white" size={25} />
-            </Buttonrounded>
-            <Buttonrounded link={'https://github.com/adityakshk499'} title={'Github'}>
-              <FaGithub color="#433f40" size={25} />
-            </Buttonrounded>
-            <Buttonrounded link={'https://drive.google.com/file/d/1-4et_7JrwE50ptwBl1uw8fYEdp3MTzKx/view?usp=share_link'} title={"Resume"}>
-              <FaWpforms color="black" size={25} />
-            </Buttonrounded>
-            <Buttonrounded link={'https://api.whatsapp.com/send?phone=919897545121'} title={'Whatsapp'}> 
-              <FaWhatsapp  color="#13eb1e" size={25} />
-            </Buttonrounded>
-        </div>
-      </div>
+          <Buttonrounded
+            link={"https://www.linkedin.com/in/amit-paswan-04134a23b"}
+            title={"Linkedin"}>
+            <FaLinkedinIn color="white" size={25} />{" "}
+          </Buttonrounded>{" "}
+          <Buttonrounded
+            link={"https://github.com/AmitPaswan18"}
+            title={"Github"}>
+            <FaGithub color="#433f40" size={25} />{" "}
+          </Buttonrounded>{" "}
+          <Buttonrounded
+            link={"https://forms.gle/EHRGkEoVpv4e6Dak8"}
+            title={"Resume"}>
+            <FaWpforms color="black" size={25} />{" "}
+          </Buttonrounded>{" "}
+          <Buttonrounded
+            link={"https://api.whatsapp.com/send?phone=918840932780"}
+            title={"Whatsapp"}>
+            <FaWhatsapp color="#13eb1e" size={25} />{" "}
+          </Buttonrounded>{" "}
+        </div>{" "}
+      </div>{" "}
     </>
   );
 };
